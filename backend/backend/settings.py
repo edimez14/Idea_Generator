844c0b51b653f463e43b917d7a2639d1ee5ec576
edimez14 <edimez14@gmail.com>
2025-01-15 18:33:35 -0500

    update file settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
 }
 
 if not DEBUG:
-    ALLOWED_HOSTS = ["*"]
+    ALLOWED_HOSTS = ["https://idea-generator-pb5d.onrender.com/"]
 
     RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
     if RENDER_EXTERNAL_HOSTNAME:
